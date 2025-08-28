cb83a8d6c679f64d39b0a3bc92c13d789ac8581e
favibe <ibefavour85@gmail.com>
2025-08-28 10:02:11 +0100

    sumall

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let start = Math.min(a,b);
+    let end = Math.max(a,b);
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum = sum +i;
+    }
+    return sum;
 };
 
 // Do not edit below this line

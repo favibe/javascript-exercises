7c8e95a7279f2c4a2be573e893d1cfaa0ef89564
favibe <ibefavour85@gmail.com>
2025-08-28 08:58:02 +0100

    repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
 const repeatString = function(string, num) {
-
+    let results = "";
+    for (let i = 0; i < num; i++) {
+        results = results + string;
+    }
+    return results;
 };
 
 // Do not edit below this line

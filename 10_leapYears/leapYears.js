8710bf69de34316bf183afd384935e7dc50cc6f9
favibe <ibefavour85@gmail.com>
2025-08-28 10:08:13 +0100

    leap year

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+     if (year % 400 === 0) {
+    return true;   // Rule 1
+  } else if (year % 100 === 0) {
+    return false;  // Rule 2
+  } else if (year % 4 === 0) {
+    return true;   // Rule 3
+  } else {
+    return false;  // Rule 4
+  }
 };
 
 // Do not edit below this line
